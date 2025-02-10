21ad9f4fbef5990a58e6ef497772825f1c98cd09
Niraj Yadav <niryadav@redhat.com>
2025-02-10 20:45:54 +0530

    Change CWD to file's directory if PWD is different
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- If file's base directory is different, CD to it.
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local file_dir = vim.fn.expand("%:p:h")
+    local pwd = vim.fn.getcwd()
+
+    if file_dir ~= pwd and vim.fn.isdirectory(file_dir) == 1 then
+      vim.cmd("lcd" .. file_dir)
+      vim.api.nvim_notify("Changed cwd to " .. file_dir, vim.log.levels.INFO, { title = "Auto CD" })
+    end
+  end,
+})
