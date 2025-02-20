ea2062457e0a27b08774ee6f43c9e7353978b71c
Niraj Yadav <niryadav@redhat.com>
2025-02-20 20:32:46 +0530

    Use TMUX as OSC52 provider
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,25 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Do not use system registers for nvim
+-- Do not use system registers as nvim unnamedplus register
 vim.opt.clipboard = ""
-
--- Enable OSC 52 provider, make sure the option is enabled
--- in the terminal emulator of your choice
---
--- Additionally, it is only enabled if vscode env is not found
--- This helps in cases where user is using the same nvim config
--- for both VSCode and Neovim
-if not vim.g.vscode then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
-  }
-end
