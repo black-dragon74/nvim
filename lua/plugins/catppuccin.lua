461087ca7ab6538d910cdefc4f4c1424a32b5a5f
Niraj Yadav <niryadav@redhat.com>
2025-09-03 12:38:47 +0530

    Added workaround for catppuccin
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,8 +9,14 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    opts = {
-      transparent_background = false,
-    },
+    -- temp fix until lazyvim is updated to new theme syntax
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      opts.transparent_background = true
+      return opts
+    end,
   },
 }
