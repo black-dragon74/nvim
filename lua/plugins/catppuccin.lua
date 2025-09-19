e16646b86143c19fc4b06ba01acdcf2e11fe1520
Niraj Yadav <niryadav@redhat.com>
2025-09-19 14:57:08 +0530

    Revert "Added workaround for catppuccin"
    This reverts commit 461087ca7ab6538d910cdefc4f4c1424a32b5a5f.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,14 +9,8 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    -- temp fix until lazyvim is updated to new theme syntax
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      opts.transparent_background = true
-      return opts
-    end,
+    opts = {
+      transparent_background = false,
+    },
   },
 }
