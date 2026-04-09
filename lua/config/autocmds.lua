ed780bb127887082bfc9551dc22d9f513baff948
Niraj Yadav <niryadav@redhat.com>
2026-04-10 04:29:15 +0530

    config: disable inlay hints and remove auto cd
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,23 +7,5 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- If file's base directory is different, CD to it.
--- Prefer git root if available
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    local file_dir = vim.fn.expand("%:p:h")
-    local git_root = vim.fn.systemlist("git -C " .. vim.fn.shellescape(file_dir) .. " rev-parse --show-toplevel")[1]
-    local pwd = vim.fn.getcwd()
-
-    local new_cwd = vim.fn.isdirectory(git_root) == 1 and git_root
-      or (vim.fn.isdirectory(file_dir) == 1 and file_dir or nil)
-
-    if new_cwd and pwd ~= new_cwd then
-      vim.cmd("lcd" .. new_cwd)
-      vim.api.nvim_echo({ { "Changed cwd to " .. new_cwd, "MoreMsg" } }, false, {})
-    end
-  end,
-})
-
 -- Disable spell check
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,22 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = {
-      gopls = {
-        settings = {
-          gopls = {
-            hints = {
-              assignVariableTypes = false,
-              compositeLiteralFields = false,
-              -- compositeLiteralTypes = false,
-              -- constantValues = false,
-              -- functionTypeParameters = false,
-              parameterNames = false,
-              -- rangeVariableTypes = false,
-            },
-          },
-        },
-      },
-    },
+    inlay_hints = { enabled = false },
   },
 }
